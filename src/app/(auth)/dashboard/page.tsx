fc58bcbca95b2885065883caf94442569a8a9622
eddiepeters00 <eddie.peters00@outlook.com>
2024-08-30 17:05:20 +0200

    Added fetch to get user details when on dashboard

--- src/app/(auth)/dashboard/page.tsx
+++ src/app/(auth)/dashboard/page.tsx
@@ -1,6 +1,5 @@
-import { Button } from "@/components/ui/button";
-import deleteSessionCookie from "@/lib/server-actions/delete-session-cookie";
 import fetchData from "@/lib/server-actions/fetch";
+import getSessionCookie from "@/lib/server-actions/get-session-cookie";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -8,5 +7,24 @@ export default async function Page() {
   const token = cookies().get("token");
   if (!token) redirect("/");
 
+  const sessionData = await getSessionCookie();
+  try {
+    const res = await fetchData({
+      path: "http://127.0.0.1:3005/api/v1/services/user",
+      method: "post",
+      data: sessionData,
+      cache: "no-store",
+    });
+
+    console.log(res);
+    if (res.error) throw res.data;
+    if (res.data.token === "missing parameter: token") {
+      throw new Error("Invalid password");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log(sessionData);
   return <main></main>;
 }
